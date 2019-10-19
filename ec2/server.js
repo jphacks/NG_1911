75741cde866f62fd0b194783825d37ab5beb89ed
Saji Ayahito <ayahito_saji@outlook.jp>
2019-10-19 13:23:06 +0900

    Add alert

--- ec2/server.js
+++ ec2/server.js
@@ -7,9 +7,12 @@ var server = app.listen(3000, function(){
 
 const STATUS_CLOSED = 0
 const STATUS_OPEN = 1
-const SYATUS_ERROR = 2
+
+const ALERT_NONE = 0
+const ALERT_BUZZ = 1
 
 var status = STATUS_CLOSED
+var alert = ALERT_NONE
 
 app.get("/api/mobile/test", function(req, res, next){
     res.json({"Hello": "World"});
@@ -17,19 +20,24 @@ app.get("/api/mobile/test", function(req, res, next){
 
 app.get("/api/mobile/connected", function(req, res, next){
     status = STATUS_OPEN
-    res.json({ "status": status });
+    res.json({ "status": status, "alert": alert });
 });
 
 app.get("/api/mobile/alert", function(req, res, next){
-  status = STATUS_ERROR
-  res.json({ "status": status });
+  alert = ALERT_BUZZ
+  res.json({ "status": status, "alert": alert });
+});
+
+app.get("/api/mobile/no-alert", function(req, res, next){
+  alert = ALERT_NONE
+  res.json({ "status": status, "alert": alert });
 });
 
 app.get("/api/mobile/finished", function(req, res, next){
   status = STATUS_CLOSED
-  res.json({ "status": status });
+  res.json({ "status": status, "alert": alert });
 });
 
 app.get("/api/pi/status", function(req, res, next){
-    res.json({ "status": status });
+    res.json({ "status": status, "alert": alert });
 });
