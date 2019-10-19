d66e689ee814c5862131ee50b4f26cbee50a55ef
Saji Ayahito <ayahito_saji@outlook.jp>
2019-10-19 11:17:03 +0900

    Add API

--- ec2/server.js
+++ ec2/server.js
@@ -5,22 +5,31 @@ var server = app.listen(3000, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
+const STATUS_CLOSED = 0
+const STATUS_OPEN = 1
+const SYATUS_ERROR = 2
+
+var status = STATUS_CLOSED
+
 app.get("/api/mobile/test", function(req, res, next){
     res.json({"Hello": "World"});
 });
 
 app.get("/api/mobile/connected", function(req, res, next){
-    res.json({"Hello": "World"});
+    status = STATUS_OPEN
+    res.json({ "status": status });
 });
 
 app.get("/api/mobile/alert", function(req, res, next){
-    res.json({"Hello": "World"});
+  status = STATUS_ERROR
+  res.json({ "status": status });
 });
 
 app.get("/api/mobile/finished", function(req, res, next){
-    res.json({"Hello": "World"});
+  status = STATUS_CLOSED
+  res.json({ "status": status });
 });
 
 app.get("/api/pi/status", function(req, res, next){
-    res.json({"Hello": "World"});
+    res.json({ "status": status });
 });
